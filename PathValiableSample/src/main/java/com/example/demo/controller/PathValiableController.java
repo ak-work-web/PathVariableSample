a01469009f6a448c9ee002b42ff843c809465b33
ak-work-web <waku2work.it20@gmail.com>
2026-05-03 22:58:08 +0900

    feat: セクション7-3 PathVariableとボタン判別機能の実装完了

--- PathValiableSample/src/main/java/com/example/demo/controller/PathValiableController.java
+++ PathValiableSample/src/main/java/com/example/demo/controller/PathValiableController.java
@@ -2,6 +2,8 @@ package com.example.demo.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class PathValiableController {
@@ -11,4 +13,52 @@ public class PathValiableController {
 		//戻り値は「ビュー名」を返す
 		return "show";
 	}
+	
+	//GETかつ[/functon/{no}]
+	//※{no}は動的に値が変わる。
+	@GetMapping("/function/{no}")
+	public String selectFunction(@PathVariable("no") Integer no) {
+		//「ビュー名」の初期化
+		String view = null;
+		switch(no) {
+		case 1:
+			view = "pathvaliable/function1";
+			break;
+		case 2:
+			view = "pathvaliable/function2";
+			break;
+		case 3:
+			view = "pathvaliable/function3";
+			break;
+		}
+		//戻り値は「ビュー名」を返す
+		return view;
+	}
+	
+	/** 「ボタンA」押下処理 */
+	@PostMapping(value = "send", params = "a")
+	public String showAView() {
+		//戻り値は「ビュー名」を返す
+		return "submit/a";
+	}
+	
+	/** 「ボタンB」押下処理 */
+	@PostMapping(value = "send", params = "b")
+	public String showBView() {
+		//戻り値は「ビュー名」を返す
+		return "submit/b";
+	}
+	
+	/** 「ボタンC」押下処理 */
+	@PostMapping(value = "send", params = "c")
+	public String showCView() {
+		//戻り値は「ビュー名」を返す
+		return "submit/c";
+	}
+	
+	
+	
+	
+	
+	
 }
